cf76070a31159156302f14ffee2ae7d0368363b0
Michael Walle <michael.walle@kontron.com>
2011-12-01 11:43:26 +0100

    fumi: add logger object
    Signed-off-by: Michael Walle <michael.walle@kontron.com>

--- pyhpi/fumi.py
+++ pyhpi/fumi.py
@@ -1,7 +1,11 @@
 import time
+import logging
 
 from sahpi import *
 from utils import TextBuffer, BaseHpiObject
+
+logger = logging.getLogger(__name__)
+
 
 class SourceInfo(BaseHpiObject):
     def from_ctype(self, s):
@@ -17,6 +21,60 @@ class SourceInfo(BaseHpiObject):
 
         return self
 
+
+class TargetInfo(BaseHpiObject):
+    def from_ctype(self, s):
+        BaseHpiObject.from_ctype(self, s)
+
+        self.id = s.BankId
+        self.size = s.BankSize
+        self.position = s.Position
+        self.state = s.BankState.value
+        self.identifier = TextBuffer().from_ctype(s.Identifier)
+        self.description = TextBuffer().from_ctype(s.Description)
+        self.date_time = TextBuffer().from_ctype(s.DateTime)
+        self.major_version = s.MajorVersion
+        self.minor_version = s.MinorVersion
+        self.aux_version = s.AuxVersion
+
+        return self
+
+
+class FirmwareInstance(BaseHpiObject):
+    def from_ctype(self, s):
+        BaseHpiObject.from_ctype(self, s)
+
+        self.is_present = bool(s.InstancePresent)
+        self.identifier = TextBuffer().from_ctype(s.Identifier)
+        self.description = TextBuffer().from_ctype(s.Description)
+        self.date_time = TextBuffer().from_ctype(s.DateTime)
+        self.major_version = s.MajorVersion
+        self.minor_version = s.MinorVersion
+        self.aux_version = s.AuxVersion
+
+        return self
+
+
+class LogicalTargetInfo(BaseHpiObject):
+    def from_ctype(self, s):
+        BaseHpiObject.from_ctype(self, s)
+
+        self.fw_persistent_location_count = s.FirmwarePersistentLocationCount
+        self.state = s.BankStateFlags
+        self.pending_instance = \
+                FirmwareInstance().from_ctype(s.PendingFwInstance)
+        self.rollback_instance = \
+                FirmwareInstance().from_ctype(s.RollbackFwInstance)
+
+        # remove instances which are not present
+        if not self.pending_instance.is_present:
+            self.pending_instance = None
+        if not self.rollback_instance.is_present:
+            self.rollback_instance = None
+
+        return self
+
+
 class FumiBank(object):
     def __init__(self, session, resource, fumi, bank_num):
         self.session = session
@@ -25,7 +83,7 @@ class FumiBank(object):
         self._as_parameter_ = SaHpiBankNumT(bank_num)
 
     def set_source(self, uri):
-        uri = TextBuffer(uri)
+        uri = TextBuffer().from_string(uri)
         saHpiFumiSourceSet(self.session, self.resource, self.fumi, self,
                 byref(uri.to_ctype()))
 
@@ -40,12 +98,27 @@ class FumiBank(object):
 
         return status.value
 
+    def cancel(self):
+        saHpiFumiUpgradeCancel(self.session, self.resource, self.fumi,
+                self)
+
     def source_info(self):
         info = SaHpiFumiSourceInfoT()
         saHpiFumiSourceInfoGet(self.session, self.resource, self.fumi,
                 self, byref(info))
 
         return  SourceInfo().from_ctype(info)
+
+    def bank_info(self):
+        _info = SaHpiFumiBankInfoT()
+        saHpiFumiTargetInfoGet(self.session, self.resource, self.fumi,
+                self, byref(_info))
+
+        info = TargetInfo().from_ctype(_info)
+        logger.debug('Fetched target info for (%d/%d): %s',
+            self.fumi._as_parameter_.value, self._as_parameter_.value, info)
+
+        return info
 
     def start_installation(self):
         saHpiFumiInstallStart(self.session, self.resource, self.fumi, self)
@@ -65,6 +138,17 @@ class Fumi(object):
 
     def bank_number(self, num):
         return FumiBank(self.session, self.resource, self, num)
+
+    def logical_bank_info(self):
+        _info = SaHpiFumiLogicalBankInfoT()
+        saHpiFumiLogicalTargetInfoGet(self.session, self.resource, self,
+                byref(_info))
+
+        info = LogicalTargetInfo().from_ctype(_info)
+        logger.debug('Fetched logical target info for (%d): %s',
+            self._as_parameter_.value, info)
+
+        return info
 
     def start_activation(self, logical=True):
         saHpiFumiActivateStart(self.session, self.resource, self, logical)
